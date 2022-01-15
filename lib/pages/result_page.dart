da7bbc370b950b9f7d98efc0d6e7f85e33b90119
Gavin <gavinmcd94@gmail.com>
2022-01-15 18:16:33 +0000

    check internet connectivity on results

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -7,6 +7,9 @@ import 'package:irish_bus_refresh/models/stop.dart';
 import 'package:irish_bus_refresh/services/prefs.dart';
 import 'package:irish_bus_refresh/widgets/result_tile.dart';
 import 'package:provider/provider.dart';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+
 class ResultPage extends StatefulWidget {
   const ResultPage({Key key, @required this.stop}) : super(key: key);
 
@@ -17,14 +20,15 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
-
   List<Widget> _results = [];
   final _client = http.Client();
+  bool hasConnection = true;
+  
 
   @override
-  initState(){
+  initState() {
     getStopData(widget.stop.name);
-    print('${widget.stop.stopNumber}');
+    checkForInternet();
     super.initState();
   }
 
@@ -36,7 +40,7 @@ class _ResultPageState extends State<ResultPage> {
     return Scaffold(
       appBar: AppBar(
         actions: [
-            IconButton(
+          IconButton(
             padding: const EdgeInsets.all(0),
             icon: prefs.favourites.contains(widget.stop)
                 ? const Icon(
@@ -54,10 +58,43 @@ class _ResultPageState extends State<ResultPage> {
         ],
         title: Text(widget.stop.name.split(", ")[0]),
       ),
-      body: _results.isEmpty & !_loading ? const Text('No Busses :(') : Center(
-        child: _loading ? const CircularProgressIndicator() : ListView(children: _results,)
-      ),
+      body: buildBody()
     );
+  }
+
+  buildBody(){
+    if(!hasConnection){
+      return const Center(child: Text('No Internet Connection'),);
+    }
+    if(_results.isEmpty & !_loading){
+      return const Center(child: Text('No buses due'),);
+    }
+
+    else {
+      return Center(
+              child: _loading
+                  ? const CircularProgressIndicator()
+                  : ListView(
+                      children: _results,
+                    ));
+    }
+  }
+
+  Future<bool> checkForInternet() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile) {
+      setState(() {
+        hasConnection = true;
+      });
+    } else if (connectivityResult == ConnectivityResult.wifi) {
+      setState(() {
+        hasConnection = true;
+      });
+    } else {
+      setState(() {
+        hasConnection = false;
+      });
+    }
   }
 
   Future<void> getStopData(String stopId) async {
@@ -70,13 +107,11 @@ class _ResultPageState extends State<ResultPage> {
     int day = DateTime.now().day;
 
     String monthString = month < 10 ? "0$month" : month.toString();
-
-    print(monthString);
-    /////////THIS IS THE WORKING CODE FOR RETRIEVING RESULTS FROM SMARTDUBLIN
     try {
-      final response = await _client.get('https://journeyplanner.transportforireland.ie/nta/XML_DM_REQUEST?coordOutputFormat=WGS84%5Bdd.ddddd%5D&language=ie&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&includeCompleteStopSeq=1&mergeDep=1&mode=direct&useAllStops=1&type_dm=any&nameInfo_dm=$stopId&itdDateDayMonthYear=$day.$monthString.$year&itdLPxx_snippet=1&itdLPxx_template=dmresults&outputFormat=rapidJSON');
-      print('https://journeyplanner.transportforireland.ie/nta/XML_DM_REQUEST?coordOutputFormat=WGS84%5Bdd.ddddd%5D&language=ie&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&includeCompleteStopSeq=1&mergeDep=1&mode=direct&useAllStops=1&type_dm=any&nameInfo_dm=$stopId&itdDateDayMonthYear=$day.$monthString.$year&itdLPxx_snippet=1&itdLPxx_template=dmresults&outputFormat=rapidJSON');
-      
+      final response = await _client.get(
+          'https://journeyplanner.transportforireland.ie/nta/XML_DM_REQUEST?coordOutputFormat=WGS84%5Bdd.ddddd%5D&language=ie&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&includeCompleteStopSeq=1&mergeDep=1&mode=direct&useAllStops=1&type_dm=any&nameInfo_dm=$stopId&itdDateDayMonthYear=$day.$monthString.$year&itdLPxx_snippet=1&itdLPxx_template=dmresults&outputFormat=rapidJSON');
+     
+
       if (response.statusCode == 200) {
         var parsed = json.decode(response.body);
 
@@ -84,7 +119,7 @@ class _ResultPageState extends State<ResultPage> {
 
         print(parsed[''].runtimeType);
 
-        if(parsed['stopEvents'] == null){
+        if (parsed['stopEvents'] == null) {
           setState(() {
             _loading = false;
             _results = [];
@@ -95,20 +130,20 @@ class _ResultPageState extends State<ResultPage> {
           int min = DateTime.parse(result['departureTimePlanned']).minute;
           String minute = min < 10 ? '0$min' : '$min';
           _results.add(ResultTile(
-            departureTime: '${DateTime.parse(result['departureTimePlanned']).hour}:$minute',
-            destination: result['transportation']['destination']['name'],
-            route: result['transportation']['disassembledName']
-          ));
+              departureTime:
+                  '${DateTime.parse(result['departureTimePlanned']).hour}:$minute',
+              destination: result['transportation']['destination']['name'],
+              route: result['transportation']['disassembledName']));
         }
 
         setState(() {
           _results = _results;
           _loading = false;
         });
-
       } else {
         setState(() {
           _loading = false;
+          hasConnection = false;
         });
         throw Exception("Failed to Load Station Data");
       }
@@ -124,9 +159,4 @@ class _ResultPageState extends State<ResultPage> {
       // notifyListeners();
     }
   }
-
-
 }
-
-
-
